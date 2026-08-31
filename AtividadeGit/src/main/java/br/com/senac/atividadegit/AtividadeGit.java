5c68cc1d8a64a7d995f1cc5de8ad766dabbd90eb
Hg <henry61623916@edu.df.senac.br>
2026-08-31 16:59:51 -0300

    Projeto de soma

---
+++ AtividadeGit/src/main/java/br/com/senac/atividadegit/AtividadeGit.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.com.senac.atividadegit;
+
+/**
+ *
+ * @author henry61623916
+ */
+public class AtividadeGit {
+
+    public static void main(String[] args) {
+        int numero1;
+        int numero2;
+        int soma;
+        
+        numero1 = 2;
+        numero2 = 4;
+        
+        soma = numero1 + numero2;
+        
+        System.out.println("O resultado é: " + soma);
+       
+    }
+}
